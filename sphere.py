f8682c1bb846884da8574df0a9562f52a9744def
powyy09 <149069194+powyy09@users.noreply.github.com>
2024-02-17 17:20:00 +0545

    update sphere.py

--- sphere.py
+++ sphere.py
@@ -1,9 +1,18 @@
-class circle:
+class Circle:
 
     def __init__(self, r):
         self.r = r
-        print (f"area is {self.area} and volume is {self.volume}")
+
+    def volume(self):
+        return 4/3*3.14*self.r*self.r*self.r
 
     def area(self):
-        return 
+        return 4*3.14*self.r*self.r
+
+
+circle = Circle(2)
+circle2 = Circle(3)
+
+print(circle.area());
+
 
