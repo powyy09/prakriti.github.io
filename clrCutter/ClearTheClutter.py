7b2cb9b41045aed2aa01783becdbcdf97c5eecf3
powyy09 <kattelprakriti@gmail.com>
2024-04-05 18:37:16 +0545

    5th April

---
+++ clrCutter/ClearTheClutter.py
@@ -0,0 +1,19 @@
+import os
+
+mainDir = "C:\\Users\\katte\\Documents\\Assets\\Python\\clrCutter\\dummyfiles"
+ext = "png"
+destDir = "C:\\Users\\katte\\Documents\\Assets\\Python\\clrCutter\\dest" 
+
+def sortfiles(ext, mainDir, destDir):
+    files = os.listdir(mainDir)
+    print(f"this is the unsorted files: {files}")
+    n= 0
+    for file in files:
+        os.rename(mainDir+ "\\"+ file , destDir + "\\" + str(n) + "." + ext  )
+        n= n+1
+        
+    
+
+sortfiles(ext, mainDir)
+
+
