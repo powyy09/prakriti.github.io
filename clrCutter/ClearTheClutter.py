99e0973d4b294d4407b40c48a643300cce797258
powyy09 <kattelprakriti@gmail.com>
2024-04-08 10:56:21 +0545

    msg

--- clrCutter/ClearTheClutter.py
+++ clrCutter/ClearTheClutter.py
@@ -1,18 +1,27 @@
 import os
 
-mainDir = "C:\\Users\\katte\\Documents\\Assets\\Python\\clrCutter\\dummyfiles"
-ext = "png"
-destDir = "C:\\Users\\katte\\Documents\\Assets\\Python\\clrCutter\\dest" 
-
-def sortfiles(ext, mainDir, destDir):
+def sortfiles(ext, mainDir):
     files = os.listdir(mainDir)
     print(f"this is the unsorted files: {files}")
-    n= 0
+    count = 0
     for file in files:
-        os.rename(mainDir+ "\\"+ file , destDir + "\\" + str(n) + "." + ext  )
-        n= n+1
-         
-
-sortfiles(ext, mainDir)
+        if((file.split(".")[1]) == ext): 
+            os.rename(mainDir+ "\\"+ file , mainDir + "\\" + str(count) + "." + ext  )
+            count = count + 1
 
 
+
+mainDir = "C:\\Users\\katte\\Documents\\Assets\\Python\\clrCutter\\dummyfiles"
+files = os.listdir(mainDir)
+fileTypes = set()
+for file in files:
+    fileTypes.add(file.split(".")[1])
+print(fileTypes)
+
+ext = input("Enter the file type to be sorted")
+
+if(ext in fileTypes):
+    sortfiles(ext,mainDir)
+else:
+    print("This type of File is not availabe in this Directory")
+    
---
+++ mergePDF/mergePDF.py
@@ -0,0 +1,15 @@
+import pypdf
+
+from pypdf import PdfMerger 
+hello = "C:\\Users\\katte\\Documents\\Assets\\Python\\mergePDF\\hello.pdf"
+hiee = "C:\\Users\\katte\\Documents\\Assets\\Python\\mergePDF\\hiee.pdf"
+
+pdfs = [hello, hiee]
+merger =  PdfMerger ()
+
+for pdf in pdfs :
+    merger.append(pdf)
+    
+mergedpdf = "C:\\Users\\katte\\Documents\\Assets\\Python\\mergePDF\\mergedpdf.pdf"
+
+merger.write(mergedpdf)
